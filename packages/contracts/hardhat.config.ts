67481579d435c36d0496616b1e05c91768e1d457
bobetbat <b.bobetayev.97@gmail.com>
2024-06-01 17:19:33 +0200

    chore: init contracts

---
+++ packages/contracts/hardhat.config.ts
@@ -0,0 +1,44 @@
+import { HardhatUserConfig } from "hardhat/config";
+import { mantle, optimism, linea, polygonZkEvm } from 'wagmi/chains'
+import "@nomicfoundation/hardhat-toolbox";
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const config: HardhatUserConfig = {
+  defaultNetwork: "sepolia",
+  networks: {
+    // localhost: {
+    //   url: "http://127.0.0.1:8545"
+    // },
+    // sepolia: {
+    //   url: process.env.SEPOLIA_RPC_URL,
+    //   accounts: [process.env.PRIVATE_KEY ?? ""]
+    // },
+    zircuit: {
+      url: "https://zircuit1.p2pify.com",
+      accounts: [process.env.PRIVATE_KEY ?? ""]
+    },
+    mantle: {
+      url: mantle.rpcUrls.default.http[0],
+      accounts: [process.env.PRIVATE_KEY ?? ""]
+    },
+    polygonZkEvm: {
+      url: polygonZkEvm.rpcUrls.default.http[0],
+      accounts: [process.env.PRIVATE_KEY ?? ""],
+    },
+
+    optimism: {
+      url: optimism.rpcUrls.default.http[0],
+      accounts: [process.env.PRIVATE_KEY ?? ""],
+    },
+
+    linea: {
+      url: linea.rpcUrls.default.http[0],
+      accounts: [process.env.PRIVATE_KEY ?? ""],
+    }
+  },
+  solidity: "0.8.20",
+};
+
+export default config;
